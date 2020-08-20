9610c68b653ed28e13d4a531fcb21830fea5f1a1
jackpan <826647196@qq.com>
2020-08-20 13:13:12 +0800

    Add modal

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,52 +2,63 @@
 <div id="loginPage">
     <div id="loginApp">
     <img src="../assets/Simbol_Konohagakure.png">
-      <div>
+        <div>
         <p id="titleContent">
-          QW投票问卷
+            QW投票问卷
         </p>
-      </div>
-    <div id="login">
-        <el-row class="inputDistance">
-            <el-col :span="6">
-                用户名: 
-            </el-col>
-            <el-col :span="12">
-               <el-input v-model="username" placeholder="请输入用户名"></el-input>
-            </el-col>
-        </el-row>
-        <el-row class="inputDistance">
-            <el-col :span="6">
-                密码: 
-            </el-col>
-            <el-col :span="12">
-               <el-input placeholder="请输入密码" v-model="userpassword" show-password></el-input>
-            </el-col>
+        </div>
+        <Modal :show="show" :title="title" @hideModal="hideModal" @submit="submit">
+            <p>{{ message }}</p>
+        </Modal>
+        <div id="login">
+            <el-row class="inputDistance">
+                <el-col :span="6">
+                    用户名: 
+                </el-col>
+                <el-col :span="12">
+                    <el-input v-model="username" placeholder="请输入用户名"></el-input>
+                </el-col>
+            </el-row>
+            <el-row class="inputDistance">
+                <el-col :span="6">
+                    密码: 
+                </el-col>
+                <el-col :span="12">
+                    <el-input placeholder="请输入密码" v-model="userpassword" show-password></el-input>
+                </el-col>
+                
+            </el-row>
+            <el-row class="inputDistance">
+                <el-col :span="12">
+                    <el-button type="primary" v-on:click="login">登陆</el-button>
+                </el-col>
+                <el-col :span="12">
+                    <router-link to="/register"><el-button>注册</el-button></router-link>
+                </el-col>
+                <p></p>
+            </el-row>
             
-        </el-row>
-        <el-row class="inputDistance">
-            <el-col :span="12">
-                <el-button type="primary" v-on:click="login">登陆</el-button>
-            </el-col>
-            <el-col :span="12">
-                <router-link to="/register"><el-button>注册</el-button></router-link>
-            </el-col>
-            <p></p>
-        </el-row>
-        
+        </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
+import Modal from './Modal.vue'
 export default {
     name: 'login',
     data() {
         return {
             username: '',
-            userpassword: ''
+            userpassword: '',
+            title: '友情提示',
+            show: false,
+            message: ''
         }
+    },
+
+    components: {
+        Modal
     },
 
     methods: {
@@ -61,6 +72,10 @@ export default {
                     console.log(response)
                 }
             ).catch((error) => {
+                if (error.response.status === 401) {
+                    this.message = '账号或者密码错误',
+                    this.show = true
+                }
                 if (error.response) {
                     console.log(error.response.data);
                     console.log(error.response.status);
@@ -68,6 +83,16 @@ export default {
                 }
                 
             })
+        },
+
+        hideModal() {
+            // 取消弹窗回调
+            this.show = false
+        },
+
+        submit() {
+            // 确认弹窗回调
+            this.show = false
         }
     }
 }
